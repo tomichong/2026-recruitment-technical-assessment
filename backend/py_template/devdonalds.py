2408107965df12cd1e9c9e822f084b8301a38629
Tomi Chong <z5542085@ad.unsw.edu.au>
2026-02-14 17:51:08 +1100

    task 3

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -106,10 +106,9 @@ def isIngredientValid(entry):
 
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# HELLO! this was written with some assumptions of common sense, to hopefully avoid human error
+	# HELLO! this was written with some additional assumptions, to hopefully avoid human error
 	# from whoever is making the post request. this could include quantity having to be a +ve int only,
-	# or no repeated requiredItems (just have one requiredItem with a higher quantity)
-
+	# or no repeated requiredItems in a given entry (cake entry can't have 1 egg and 1 more egg)
 	entry = request.json
 
 	# every entry must have a name
@@ -124,6 +123,7 @@ def create_entry():
 	elif entryName in cookbook:
 		# don't allow adding an entry already present in the cookbook
 		return 'entry already exists in cookbook', 400
+
 	# every entry must have a type
 	entryType = entry.get('type')
 	if not entryType:
@@ -147,16 +147,99 @@ def create_entry():
 
 	# add to cookbook
 	cookbook[entryName] = entry
+
+	# technically i could also check if any other keys exist but aren't supposed to be there
+	# but i choose not to do this because maybe other keys will be added in the future (potentially)
 	
 	return 'success', 200
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
+
+# handle a requiredItem with this function to calculate cookTime and gather an ingredient list
+# returns totalCookTime and ingredients list
+def handleItemInfo(name, quantity):
+	if name not in cookbook:
+		# i guess the easiest way to handle invalid names here is to raise an error and let the 
+		# parent function handle check for it
+		raise ValueError('recipe name not found in cookbook')
+	
+	# if found in the cookbook then great, handle it
+	entry = cookbook[name]
+
+	# handle ingredient (easy part)
+	if entry['type'] == 'ingredient':
+		totalCookTime = entry['cookTime'] * quantity
+		return totalCookTime, {name: quantity}
+
+	# expect that if the entry is not an ingredient then it must be a recipe
+	# if a new type is added then tests added in the future should fail here
+	if entry['type'] != 'recipe':
+		raise ValueError("what? there's a new entry type and you forgot to handle it here..")
+
+	# handle recipe recursively
+	totalCookTime = 0
+	ingredients = {}
+
+	for item in entry.get('requiredItems', []):
+		itemName = item['name']
+		itemQuantity = item['quantity'] * quantity
+
+		# recursion here is fine because we don't expect there to be hundreds or thousands of nested
+		# recipes in any given recipe. if this is expected to scale then an iterative solution with
+		# a queue might be better here, but for right now practically a recipe should be fine to recurse.
+
+		# further, there could be a case where loops could occur. i guess i can assume that recipes
+		# would not have loops, but if i was writing a more rigorous app prone to human error, then
+		# i would want to check that we don't recurse items that have already been seen, to prevent
+		# infinite loops
+
+		# any errors here would propogate to parent function
+		itemCookTime, itemIngredients = handleItemInfo(itemName, itemQuantity)
+
+		# add subrecipe's cooktime and ingredients to the result
+		totalCookTime += itemCookTime
+		for itemIngredientName, itemIngredientQuantity in itemIngredients.items():
+			# add to ingredients dict
+			ingredients[itemIngredientName] = ingredients.get(itemIngredientName, 0) + itemIngredientQuantity
+		
+		return totalCookTime, ingredients
+
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	name = request.args['name']
+
+	if name not in cookbook:
+		# check if this recipe exists in the cookbook
+		return 'recipe not found in cookbook', 400
+
+	elif cookbook[name]['type'] != 'recipe':
+		# the given name must be a recipe in the cookbook
+		return 'given name is not a recipe in the cookbook', 400
+
+	recipe = cookbook[name]
+	totalCookTime = 0
+	ingredients = {}
+
+	# recurse through the recipe
+	try:
+		totalCookTime, ingredients = handleItemInfo(name, 1)
+	except Exception as e:
+		return str(e), 400
+
+	# map the ingredients to the correct output format
+	ingredientList = []
+	for ingredientName, ingredientQuantity in ingredients.items():
+		ingredientList.append({'name': ingredientName, 'quantity': ingredientQuantity})
+
+	result = jsonify({
+		'name': name,
+		'cookTime': totalCookTime,
+		'ingredients': ingredientList
+	})
+
+	return result, 200
 
 
 # =============================================================================
